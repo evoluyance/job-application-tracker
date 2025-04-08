eee915bd90607a4e4eb819e0e6f404f67123fba1
NazarHome <torbak03@gmail.com>
2025-04-08 14:01:37 +0300

    Complete a basic JobRequest and User model functional

--- job-application-tracker/src/main/java/com/jobtracker/model/JobRequest.java
+++ job-application-tracker/src/main/java/com/jobtracker/model/JobRequest.java
@@ -1,6 +1,47 @@
 package com.jobtracker.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "job_request")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class JobRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @NotBlank
+    @Column(name= "name_job", nullable = false,length = 100)
+    private String nameJob;
+
+    @Column(name = "company_name", length = 100)
+    private String companyName;
+
+    @Column(name = "comment_of_job", columnDefinition = "TEXT")
+    private String commentOfJob;
+
+    @Column(name = "url_job", length = 500)
+    private String urlJob;
+
+    @Column(name = "url_resume", length = 500)
+    private String urlResume;
+
+    @Column(name = "data_job_request")
+    private LocalDateTime dataJobRequest;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status_review", nullable = false)
+    private StatusReview statusReview;
+
 }
--- job-application-tracker/src/main/java/com/jobtracker/model/User.java
+++ job-application-tracker/src/main/java/com/jobtracker/model/User.java
@@ -1,6 +1,50 @@
 package com.jobtracker.model;
 
-import jakarta.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.List;
+
+
+@Entity
+@Table(name="users")
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Pattern(regexp = "[A-Z][a-z]+([-'][A-Z][a-z]+)*", message ="username must be starting of the capital letter and" +
+            "contain only letter " )
+    @Column(nullable = false, length = 50)
+    private String username;
+
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()\\-_=+\\[\\]{}|;:'\",.<>?/`~\\\\]).{8,}$",
+            message = "Password must be at least 8 characters long and " +
+                    "contain at least one letter, one number, and " +
+                    "can include special characters"
+    )
+    @JsonIgnore
+    @Column(nullable = false)
+    private String password;
+
+    @NotBlank(message = "Must be a valid e-mail address")
+    @Email(message = "Email має бути валідним")
+    @Column(unique = true, nullable = false)
+    private String userEmail;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<JobRequest> userJobRequestList;
+
 }
